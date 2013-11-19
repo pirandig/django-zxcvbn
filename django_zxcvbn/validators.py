443522774295993083715cd865eb4bad90dedac6
Igor Gai <igor.gai@i.ua>
2013-11-19 16:55:24 +0200

    bugfix for crashing while generating error messages in LengthValidator class

--- django_zxcvbn/validators.py
+++ django_zxcvbn/validators.py
@@ -9,8 +9,8 @@ PASSWORD_MIN_LENGTH = getattr(settings, "PASSWORD_MIN_LENGTH", 8)   # This is a 
 PASSWORD_MAX_LENGTH = getattr(settings, "PASSWORD_MAX_LENGTH", 128) # Django auth user model is max_length 128
 PASSWORD_MIN_ENTROPY = getattr(settings, "ZXCVBN_MIN_ENTROPY", 25)
 
+
 class LengthValidator(object):
-    message = _("Password must be between %s and %s chars" % (PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH))
     code = "length"
 
     def __init__(self, min_length=None, max_length=None):
@@ -20,12 +20,13 @@ class LengthValidator(object):
     def __call__(self, value):
         if self.min_length and len(value) < self.min_length:
             raise ValidationError(
-                self.message % _("Must be %s characters or more") % self.min_length,
+                message=_("Must be %s characters or more") % self.min_length,
                 code=self.code)
         elif self.max_length and len(value) > self.max_length:
             raise ValidationError(
-                self.message % _("Must be %s characters or less") % self.max_length,
+                message=_("Must be %s characters or less") % self.max_length,
                 code=self.code)
+
 
 class ZXCVBNValidator(object):
     message = _("Passwords must be at least %s characters and of sufficient complexity")
